afde9f46229697021bbdb65c8cb6b7d5ad4a9e50
FLASH15814 <ranshiv369@gmail.com>
2023-11-12 00:40:27 -0500

    first commit

---
+++ src/main/java/com/mycompany/cafetuto/CafeTuto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cafetuto;
+
+/**
+ *
+ * @author ransh
+ */
+public class CafeTuto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
